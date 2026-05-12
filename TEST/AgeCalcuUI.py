1663433f5a1749e1af1fdf17ff62f2ae49df84aa
Samflorence <73792237+Samflorence@users.noreply.github.com>
2026-05-12 17:54:43 +0800

    Update AgeCalcuUI.py

--- TEST/AgeCalcuUI.py
+++ TEST/AgeCalcuUI.py
@@ -38,7 +38,7 @@ def appeand_excel():
     middle = mname_entry.get()
     last = lname_entry.get()
     by = int(birth_entry.get())
-    age = 2006 - by 
+    age = 2026 - by 
     
     book = op.load_workbook("excelDb.xlsx")
     sheet = book.active
@@ -51,6 +51,81 @@ def appeand_excel():
     messagebox.showinfo("Success","Record added successfully!")
     wordings()
 
+def auto_populate():
+    selected = tree.focus()
+    values = tree.item(selected, "values")
+    
+    if values:
+        lname_entry.delete(0,tk.END)
+        fname_entry.delete(0,tk.END)
+        mname_entry.delete(0,tk.END)
+        birth_entry.delete(0,tk.END)
+
+        lname_entry.insert(0,values[1])
+        fname_entry.insert(0,values[2])
+        mname_entry.insert(0,values[3])
+        birth_entry.insert(0,values[4])
+        
+def update_data():
+    selected = tree.focus()
+
+    if not selected:
+        messagebox.showerror("Error", "Select a record first")
+        return
+
+    if not validation():
+        return
+    
+    values = tree.item(selected, "values")
+    record_id = int(values[0])
+
+    first = fname_entry.get()
+    middle = mname_entry.get()
+    last = lname_entry.get()
+    by = int(birth_entry.get())
+    age = 2026 - by
+
+    workbook = op.load_workbook("excelDb.xlsx")
+    sheet = workbook.active
+
+    for row in sheet.iter_rows(min_row=2):
+        if row[0].value == record_id:
+            row[1].value == last
+            row[2].value == first
+            row[3].value == middle
+            row[4].value == by
+            row[5].value == age
+
+    workbook.save("excelDb.xlsx")
+
+    messagebox.showinfo("Success", "Record updated Successfully!")
+    wordings()
+
+def delete_data():
+    selected = tree.focus()
+
+    if not selected:
+        messagebox.showerror("Error", "Select a record first")
+        return
+    
+    values = tree.item(selected, "values")
+    record_id = int(values[0])
+
+    confirm = messagebox.askyesno("Confirm", "Are you sure you want to delete this record?")
+    if not confirm:
+        return 
+    
+    workbook = op.load_workbook("excelDb.xlsx")
+    sheet = workbook.active
+
+    for i, row in enumerate(sheet.iter_rows(min_row=2), start=2):
+        if row[0].value == record_id:
+            sheet.delete_rows(i)
+            break
+    workbook.save("excelDb.xlsx")
+
+    messagebox.showinfo("Success", "Record deleted Successfully!")
+    wordings()
 
 window=tk.Tk()
 window.title("Age Calculator")
@@ -93,13 +168,13 @@ birth_entry.grid(row=4, column=1,columnspan=2,padx=(10,0))
 birthyear_label = tk.Label(genframe, text="Birth Year", font=("Poppins",10,"italic"),bg="lightgreen")
 birthyear_label.grid(row=5, column=1,columnspan=2)
 
-update_btn = tk.Button(window, text="Update",command=appeand_excel)
+update_btn = tk.Button(window, text="Update",command=update_data)
 update_btn.grid(row=6, column=2)
 
-button= tk.Button(window,text="Submit",font=("Poppins",12,"bold"),bg="lightpink",command=validation)
+button= tk.Button(window,text="Submit",font=("Poppins",12,"bold"),bg="lightpink",command=appeand_excel)
 button.grid(row=6, column=0, columnspan=6,pady=(10,20))
 
-delete_btn = tk.Button(window, text="Delete",  bg="red", fg="white")
+delete_btn = tk.Button(window, text="Delete",  bg="red", fg="white",command= delete_data)
 delete_btn.grid(row=6, column=3)
 
 tree = ttk.Treeview(window, columns=("ID","Last","First","Middle","BirthYear","Age"), show="headings")
@@ -107,6 +182,7 @@ for col in ("ID","Last","First","Middle","BirthYear","Age"):
     tree.heading(col, text=col)
 tree.grid(row=7, column=0, columnspan=4)
 
+tree.bind("<<Treeviewselect>>", auto_populate)
 wordings()
 
 window.mainloop()
